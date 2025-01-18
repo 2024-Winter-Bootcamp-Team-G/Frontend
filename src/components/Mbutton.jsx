86a57f0b0cc26513bf6b503625d67266d816b36c
jiho5191 <wlghdid2@gmail.com>
2025-01-19 04:11:56 +0900

    보드판 최최최종 수정...

--- src/components/Mbutton.jsx
+++ src/components/Mbutton.jsx
@@ -10,7 +10,7 @@ const Mbutton = ({
   const styles = {
     edit: `w-[119px] h-[49px] bg-white rounded-[10px] border-2 border-[#919191] relative shadow-[2px_3px_0px_0px_rgba(0,0,0,0.25)] before:absolute before:inset-0 before:rounded-[10px] before:shadow-[inset_4px_4px_2px_0px_rgba(255,255,255,0.25)] cursor-pointer
 active:shadow-[inset_3px_3px_1px_0px_rgba(0,0,0,0.25)]`,
-    create: `max-w-[262px] h-[50px] bg-[#bfcfef] border-4 border-[#5c5c5c]/70 flex items-center justify-center px-4 truncate relative`,
+    create: `max-w-[262px] h-[50px] bg-[#BFCFEF] border-4 border-[#5C5C5C]/70 flex items-center justify-center pr-9 truncate relative`,
   };
 
   // variant에 따라 스타일 선택 (edit, create, default)
@@ -23,26 +23,23 @@ active:shadow-[inset_3px_3px_1px_0px_rgba(0,0,0,0.25)]`,
     >
       {/* edit 버튼의 글자 */}
       {variant === 'edit' && (
-        <button
-          className="absolute inset-0 rounded-[10px] cursor-pointer flex items-center justify-center text-black
-text-[1.3vw]
-font-normal"
-          onClick={onClick} // edit 버튼은 클릭 가능
-        >
-        </button>
-          {text}
-        </div>
+        <>
+          <button
+            className="absolute inset-0 rounded-[10px] cursor-pointer flex items-center justify-center text-black
+text-md"
+            onClick={onClick} // edit 버튼은 클릭 가능
+          >
+            {text}
+          </button>
+        </>
       )}
       {/* create 버튼의 글자 및 내부 버튼 */}
       {variant === 'create' && (
         <>
-          <div className="absolute right-[2.8rem] text-black text-xl font-normal truncate">
-            나의 보드 만들기
-          </div>
-
+          나의 보드 만들기
           <button
-            onClick={onClick}
-            className="w-9 h-9 bg-white cursor-pointer absolute top-1/2 -right-3.5 transform -translate-x-1/2 -translate-y-1/2 
+            onClick={onClick} // 작은 사각형만 클릭 가능
+            className="w-9 h-9 bg-white cursor-pointer absolute top-1/2 -right-3.5 transform -translate-x-1/2 -translate-y-1/2
             flex items-center justify-center shadow-[2px_2px_1px_0px_rgba(0,0,0,0.25)] active:shadow-[inset_4px_3px_0px_0px_rgba(181,181,181,0.70)]"
           >
             {/* CSS 삼각형 */}
@@ -57,5 +54,4 @@ font-normal"
     </div>
   );
 };
-
 export default Mbutton;
--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -55,26 +55,48 @@ const Board = () => {
 
           <div
             style={{ fontSize: 'clamp(16px, 2vw, 28px)' }}
-            className="text-black text-left mx-4 ml-11 mb-6"
+            className="text-black text-left ml-11 mb-6"
           >
             (Ai가 만들어준 칭호)
           </div>
 
           {/* 보드판 */}
-          <div className="grid grid-cols-3 px-10 w-full h-full mt-5">
-            {/* 이미지 컨테이너 */}
-            <div className="relative mt-[-30px] ml-[-5%] w-[120%] bg-[#d9d9d9] rounded-[20px] aspect-square">
-              <div className="absolute top-0 left-0 w-full h-full p-[5px]">
-                <div className="w-full h-full rounded-[20px] border-[3px] border-dashed border-white flex items-center justify-center">
-                  ai이미지
-                </div>
+          <div className="grid grid-cols-3 px-10 pb-5 w-full h-full">
+            <div>
+              {/* 이미지 컨테이너 */}
+              <div className="relative w-[90%] h-[65%] bg-[#d9d9d9] rounded-[20px] aspect-square">
+                <div className="absolute top-[0.3125rem] left-[0.3125rem] w-[calc(100%-0.625rem)] h-[calc(100%-0.625rem)] rounded-[20px] border-[3px] border-dashed border-white flex items-center justify-center"></div>
               </div>
+              <Mbutton
+                text="완료하기"
+                className="absolute mt-[3%] w-12 h-[15%] max-w-[7rem] max-h-[3rem]"
+                variant="edit"
+              />
+              <Mbutton
+                text="공유하기"
+                className="absolute mt-[2%] w-12 h-[15%] max-w-[7rem] max-h-[3rem]"
+                variant="edit"
+              />
             </div>
 
-            <div className="items-center justify-center gap-2">
-              {/* 2등 컨테이너 */}
-              <div className="relative mt-[-1.8rem] ml-[18%] w-[110%] h-[55%] bg-[#aedcea] rounded-[1.25rem] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+            <div className="justify-items-center">
+              {/* 3등 컨테이너 */}
+              <div className="relative w-[95%] h-[40%] bg-[#aedcea] rounded-[1.25rem] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                 <div className="absolute top-[0.3125rem] left-[0.3125rem] w-[calc(100%-0.625rem)] h-[calc(100%-0.625rem)] rounded-[1.25rem] border-[0.1875rem] border-dashed border-white flex items-center justify-center">
+                  {/* 3등 컨테이너 텍스트 */}
+                  <span className="absolute top-[10%] left-[45%] transform -translate-x-1/2 text-black text-xl font-nomal">
+                    3위
+                  </span>
+                  {/* Retry 아이콘 */}
+                  <div className="absolute top-[10%] left-[60%] transform -translate-x-1/2">
+                    <RotatingIcon />
+                  </div>
+                </div>
+              </div>
+
+              {/* 2등 컨테이너 */}
+              <div className="relative mt-[3%] w-[95%] h-[57%] bg-[#4cb2d2] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+                <div className="absolute top-[0.3125rem] left-[0.3125rem] w-[calc(100%-0.625rem)] h-[calc(100%-0.625rem)] rounded-[20px] border-[3px] border-dashed border-white flex items-center justify-center">
                   {/* 2등 컨테이너 텍스트 */}
                   <span className="absolute top-[10%] left-[45%] transform -translate-x-1/2 text-black text-xl font-nomal">
                     2위
@@ -85,29 +107,15 @@ const Board = () => {
                   </div>
                 </div>
               </div>
-
-              {/* 1등 컨테이너 */}
-              <div className="relative mt-[0.5rem] ml-[18%] w-[110%] h-[73%] bg-[#4cb2d2] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
-                <div className="absolute top-[0.3125rem] left-[0.3125rem] w-[calc(100%-0.625rem)] h-[calc(100%-0.625rem)] rounded-[20px] border-[3px] border-dashed border-white flex items-center justify-center">
-                  {/* 1등 컨테이너 텍스트 */}
-                  <span className="absolute top-[10%] left-[45%] transform -translate-x-1/2 text-black text-xl font-nomal">
-                    1위
-                  </span>
-                  {/* Retry 아이콘 */}
-                  <div className="absolute top-[10%] left-[60%] transform -translate-x-1/2">
-                    <RotatingIcon />
-                  </div>
-                </div>
-              </div>
             </div>
 
-            <div className="items-center justify-center gap-2">
-              {/* 3등 컨테이너 */}
-              <div className="relative mt-[-1.8rem] ml-[59px] w-[80%] h-[75%] bg-[#79c6de] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+            <div className="justify-items-center">
+              {/* 1등 컨테이너 */}
+              <div className="relative mb-[3%] w-[95%] h-[62%] bg-[#79c6de] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                 <div className="absolute top-[5px] left-[5px] w-[calc(100%-10px)] h-[calc(100%-10px)] rounded-[20px] border-[3px] border-dashed border-white">
-                  {/* "3위" 텍스트 (가운데 정렬) */}
+                  {/* "1위" 텍스트 (가운데 정렬) */}
                   <span className="absolute top-[8%] left-[45%] transform -translate-x-1/2 text-black text-xl font-normal">
-                    3위
+                    1위
                   </span>
                   {/* Retry 아이콘 */}
                   <div className="absolute top-[8%] left-[65%] transform -translate-x-1/2">
@@ -121,7 +129,7 @@ const Board = () => {
               </div>
 
               {/* 4등 컨테이너 */}
-              <div className="relative mt-[0.5rem] ml-[59px] w-[80%] h-[52%] bg-[#c3dee7] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+              <div className="relative w-[95%] h-[35%] bg-[#c3dee7] rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                 <div className="absolute top-[5px] left-[5px] w-[calc(100%-10px)] h-[calc(100%-10px)] rounded-[20px] border-[3px] border-dashed border-white">
                   <span className="absolute top-[8%] left-[45%] transform -translate-x-1/2 text-black text-xl font-nomal">
                     4위
@@ -134,17 +142,6 @@ const Board = () => {
               </div>
             </div>
           </div>
-
-          <Mbutton
-            text="완료하기"
-            className="absolute bottom-[7%] left-[4.5%] w-[95px] h-[55px]"
-            variant="edit"
-          />
-          <Mbutton
-            text="공유하기"
-            className="absolute bottom-[5%] left-[4.5%] w-[95px] h-[55px]"
-            variant="edit"
-          />
         </MiniHomp>
       )}
     </Background>
