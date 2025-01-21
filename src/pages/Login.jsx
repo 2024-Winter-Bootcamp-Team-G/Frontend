7822c3a33190b5565d9c911cf175a71288bbfc20
uzifs <westa0514@naver.com>
2025-01-22 04:02:34 +0900

    feat: 로그인, 로그아웃 api 연동 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,30 +4,26 @@ import Background from '../components/Background';
 import Window from '../components/Window';
 import Input from '../components/Input';
 import Button from '../components/Button';
-import axios from 'axios';
+import api from '../api/axios_config';
 import { setCookie } from '../utils/cookie';
 
 const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(''); // 에러 메시지 상태
+  const [error, setError] = useState('');
 
-  const login = async (email, password) => {
+  const login = async () => {
     try {
-      // JSON 형식으로 전송
-      const response = await axios.post(
-        'http://localhost:8000/auth/login',
-        {
-          username: email, // 서버가 username 필드를 기대함
-          password: password,
+      const data = new URLSearchParams();
+      data.append('username', email);
+      data.append('password', password);
+
+      const response = await api.post('/auth/login', data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded', // Login 요청에서만 변경
         },
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded', // JSON 형식 전송을 위한 헤더
-          },
-        }
-      );
+      });
 
       console.log('Login successful:', response.data);
 
@@ -45,33 +41,18 @@ const Login = () => {
         sameSite: 'Strict',
       });
 
-      // // Access Token 및 Refresh Token 저장
-      // localStorage.setItem('access_token', response.data.access_token);
-      // localStorage.setItem('refresh_token', response.data.refresh_token);
-
-      // 로그인 성공 시 메인 페이지로 이동
       navigate('/');
     } catch (error) {
-      if (error.response && error.response.data.detail) {
-        // 유효성 검사 오류 처리
-        setError('이메일 또는 비밀번호가 잘못되었습니다.'); // 사용자에게 에러 메시지 표시
-      } else {
-        // 기타 오류 처리
-        setError('로그인에 실패했습니다. 다시 시도해주세요.'); // 사용자에게 에러 메시지 표시
-      }
-      console.error(
-        'Login failed:',
-        error.response ? error.response.data : error.message
-      );
+      console.error('Login failed:', error.response?.data || error.message);
+      setError('로그인 요청이 실패했습니다. 다시 시도해주세요.');
     }
   };
 
   const handleLogin = () => {
-    login(email, password);
+    login();
   };
 
   const handleSignUp = () => {
-    // 회원가입 창으로 이동
     navigate('/signup');
   };
 
@@ -79,7 +60,6 @@ const Login = () => {
     <Background>
       <Window>
         <div className="p-6 pt-12 flex">
-          {/* 왼쪽: 이미지 박스 */}
           <div
             className="w-[20rem] h-[28em] bg-[#FFFFCA] flex items-center justify-center mr-12"
             style={{
@@ -87,20 +67,18 @@ const Login = () => {
             }}
           >
             <p className="text-gray-500 text-center text-[1rem]">
-              이미지를 여기에 추가하세요
+              여기에 이미지를 추가하세요.
             </p>
           </div>
-          {/* 오른쪽: 로그인 텍스트 및 입력 */}
           <div className="w-[60%]">
             <h1 className="text-[4rem] mb-4 text-left">Login</h1>
             <p className="text-[1.5rem] mb-8 text-left">
-              사용자의 이메일과 비밀번호를 입력하여 주십시오.
+              이메일과 비밀번호를 입력해 주세요.
             </p>
 
-            {/* 이메일 입력 */}
             <div className="flex items-center mb-6">
               <label htmlFor="email" className="text-[2rem] mr-4">
-                이 메 일 :
+                이메일:
               </label>
               <Input
                 name="email"
@@ -109,38 +87,28 @@ const Login = () => {
               />
             </div>
 
-            {/* 비밀번호 입력 */}
             <div className="flex items-center mb-6">
               <label htmlFor="password" className="text-[2rem] mr-4">
-                비밀번호 :
+                비밀번호:
               </label>
               <Input
                 name="password"
                 width="60%"
                 type="password"
-                onChange={(e) => setPassword(e.target.value)} // setPassword로 수정
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
-            {/* 에러 메시지 표시 */}
             {error && <p className="text-red-500 text-[1.5rem]">{error}</p>}
           </div>
-          <div
-            className="absolute bottom-[5.5rem] left-10 w-[90%] h-[3px] bg-[#868A8E] 
-               shadow-[0_3px_0_#FFFFFF,0_-3px_0_#929598]"
-          ></div>
-        </div>
-        {/* 오른쪽 아래 버튼 */}
-        <div className="absolute bottom-4 right-4 flex gap-4">
-          {/* 회원가입 버튼 */}
-          <Button type="default" onClick={handleSignUp}>
-            회원가입
-          </Button>
-
-          {/* 로그인 버튼 */}
-          <Button type="default" onClick={handleLogin}>
-            로그인
-          </Button>
+          <div className="absolute bottom-4 right-4 flex gap-4">
+            <Button type="default" onClick={handleSignUp}>
+              회원가입
+            </Button>
+            <Button type="default" onClick={handleLogin}>
+              로그인
+            </Button>
+          </div>
         </div>
       </Window>
     </Background>
