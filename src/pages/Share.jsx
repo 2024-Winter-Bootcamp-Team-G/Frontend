9a099ce07406b5c261740d3a280ec53171455a0d
uzifs <westa0514@naver.com>
2025-01-31 03:22:06 +0900

    feat: 일치율 수정 6

--- src/pages/Share.jsx
+++ src/pages/Share.jsx
@@ -27,12 +27,12 @@ const Share = ({ onClose }) => {
   const fetchMatchRatio = async (selectedBoard) => {
     setIsLoading(true);
     setError('');
-
+    const SharedBoardId = getCookie('shared_board_id');
     try {
       const response = await api.post('/boards/match-ratio', null, {
         params: {
           board_id1: selectedBoard, // 선택한 보드 ID
-          board_id2: getCookie('shared_board_id'), // 쿠키에서 가져온 보드 ID
+          board_id2: SharedBoardId, // 쿠키에서 가져온 보드 ID
         },
         headers: {
           Authorization: `Bearer ${getCookie('access_token')}`, // 인증 토큰
