ea6268ec075b9c36c0d1eae536bde1a932682c48
jiho5191 <wlghdid2@gmail.com>
2025-01-30 21:31:43 +0900

    파비콘 위치 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 const link = document.createElement('link');
 link.rel = 'icon';
 link.type = 'image/x-icon';
-link.href = '/favicon.ico'; // favicon.ico를 루트 폴더에 두었을 경우
+link.href = '/favicon.ico';
 document.head.appendChild(link);
 
 createRoot(document.getElementById('root')).render(
